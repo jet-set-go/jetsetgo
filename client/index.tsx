70e3d1a1d18e63b0cd6d62817e75c82952b02070
Wvaviator <wvaviator@gmail.com>
2023-01-05 20:17:26 -0600

    merge conflict example

--- client/index.tsx
+++ client/index.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <App />
+    </div>
   </React.StrictMode>,
-  document.getElementById("app")
+  document.getElementById('app')
 );
