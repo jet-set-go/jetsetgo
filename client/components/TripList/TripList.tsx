cae26c496e142ca44ffa628850b695f04576bfa5
Wvaviator <wvaviator@gmail.com>
2023-01-07 14:18:09 -0600

    add delete icon button

--- client/components/TripList/TripList.tsx
+++ client/components/TripList/TripList.tsx
@@ -2,15 +2,16 @@ import {
   Box,
   Button,
   Divider,
+  IconButton,
   List,
   ListItem,
   ListItemText,
-  Paper,
   Typography,
 } from '@mui/material';
 import React from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import styles from './TripList.module.css';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { Link } from 'react-router-dom';
 
 const dummyTrips = [
@@ -35,6 +36,10 @@ const dummyTrips = [
 ];
 
 const TripList = () => {
+  const handleDelete = (id: string) => {
+    console.log('delete', id);
+  };
+
   return (
     <div className={styles.container}>
       <Box mb={2}>
@@ -43,31 +48,47 @@ const TripList = () => {
         </Button>
       </Box>
       <Divider />
-      <List sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-        {dummyTrips.map((trip) => (
-          // TODO: Link component should link to dashboard for specific trip once routing is fleshed out
-          <Link to={`/trip/${trip.id}`} className={styles.link}>
+      <Box mt={2}>
+        <List sx={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+          {dummyTrips.map((trip) => (
+            // TODO: Link component should link to dashboard for specific trip once routing is fleshed out
             <div className={styles.card}>
-              <ListItem key={trip.id}>
-                <ListItemText
-                  primary={trip.destination}
-                  secondary={
-                    <Typography
-                      sx={{ display: 'inline' }}
-                      component="span"
-                      variant="body2"
-                      color="text.primary"
-                    >
-                      {new Date(trip.startDate).toLocaleDateString()} -{' '}
-                      {new Date(trip.endDate).toLocaleDateString()}
-                    </Typography>
-                  }
-                />
-              </ListItem>
+              <Link
+                key={trip.id}
+                to={`/trip/${trip.id}`}
+                className={styles.link}
+              >
+                <ListItem key={trip.id}>
+                  <ListItemText
+                    primary={trip.destination}
+                    secondary={
+                      <Typography
+                        sx={{ display: 'inline' }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                      >
+                        {new Date(trip.startDate).toLocaleDateString()} -{' '}
+                        {new Date(trip.endDate).toLocaleDateString()}
+                      </Typography>
+                    }
+                  />
+                </ListItem>
+              </Link>
+              <IconButton
+                onClick={(e) => {
+                  e.preventDefault();
+                  console.log('stopPropagation');
+                  e.stopPropagation;
+                  handleDelete(trip.id);
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
             </div>
-          </Link>
-        ))}
-      </List>
+          ))}
+        </List>
+      </Box>
     </div>
   );
 };
