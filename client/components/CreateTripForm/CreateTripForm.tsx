7286d1e3931402fa2ce6c876ef44f6d893419a50
Wvaviator <wvaviator@gmail.com>
2023-01-09 22:17:59 -0600

    add datepickers and background

--- client/components/CreateTripForm/CreateTripForm.tsx
+++ client/components/CreateTripForm/CreateTripForm.tsx
@@ -1,7 +1,7 @@
-import { Autocomplete, AutocompleteValue, TextField } from '@mui/material';
+import { Autocomplete, TextField } from '@mui/material';
 import React, { useEffect } from 'react';
 import { Form } from 'react-router-dom';
-
+import styles from './CreateTripForm.module.css';
 interface Place {
   name: string;
   place_id: string;
@@ -41,24 +41,53 @@ const CreateTripForm = () => {
   }, [debounceDestination]);
 
   return (
-    <Form>
-      <Autocomplete
-        disablePortal
-        id="destination-input"
-        options={destinationOptions.map((option) => option.name)}
-        sx={{ width: 300 }}
-        inputValue={destination}
-        onInputChange={(event, newInputValue) => {
-          setDestination(newInputValue);
-        }}
-        onChange={(event, newValue) => {
-          console.log('newValue: ', newValue);
-        }}
-        renderInput={(params) => (
-          <TextField {...params} label="Destination" variant="outlined" />
-        )}
+    <div className={styles.container}>
+      <img
+        src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"
+        alt="beach"
+        className={styles.background}
       />
-    </Form>
+      <Form>
+        <div className={styles.form}>
+          <Autocomplete
+            disablePortal
+            id="destination-input"
+            options={destinationOptions.map((option) => option.name)}
+            sx={{ minWidth: 480 }}
+            inputValue={destination}
+            onInputChange={(event, newInputValue) => {
+              setDestination(newInputValue);
+            }}
+            onChange={(event, newValue) => {
+              console.log('newValue: ', newValue);
+            }}
+            renderInput={(params) => (
+              <TextField {...params} label="Destination" variant="outlined" />
+            )}
+          />
+          <TextField
+            id="start-date"
+            label="Trip Start"
+            type="date"
+            // defaultValue="2023-01-19"
+            sx={{ minWidth: 220 }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            id="end-date"
+            label="Trip End"
+            type="date"
+            // defaultValue="2017-05-24"
+            sx={{ minWidth: 220 }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+      </Form>
+    </div>
   );
 };
 
