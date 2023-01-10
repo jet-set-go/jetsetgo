d5989471e0c7123e8ebeda56f7cbb683bedbf5a6
Wvaviator <wvaviator@gmail.com>
2023-01-09 21:17:54 -0600

    update trip schema

--- src/models/trip.ts
+++ src/models/trip.ts
@@ -2,12 +2,40 @@ import mongoose from 'mongoose';
 
 export interface ITrip {
   name: string;
-  destination: string;
+  destination: {
+    name: string;
+    place_id: string;
+    location: {
+      lat: number;
+      lng: number;
+    };
+  };
+  packingList: {
+    name: string;
+    checked: boolean;
+  }[];
+  startDate: Date;
+  endDate: Date;
 }
 
 const tripSchema = new mongoose.Schema<ITrip>({
   name: { type: String, required: true },
-  destination: { type: String, required: true },
+  destination: {
+    name: { type: String, required: true },
+    place_id: { type: String, required: true },
+    location: {
+      lat: { type: Number, required: true },
+      lng: { type: Number, required: true },
+    },
+  },
+  packingList: [
+    {
+      name: { type: String, required: true },
+      checked: { type: Boolean, required: true },
+    },
+  ],
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
 });
 
 const Trip =
