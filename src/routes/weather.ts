ef76e5d58c3dcb131982688057fd6906bbf467eb
ryan <gauserr@icloud.com>
2023-01-11 04:40:19 -0500

    formatted

--- src/routes/weather.ts
+++ src/routes/weather.ts
@@ -1,7 +1,7 @@
 import { Router, Response, Request, NextFunction } from 'express'
 import weatherController from '../controllers/weatherController'
 const router = Router()
-router.get('/', weatherController, (req, res) => {
+router.get('/', weatherController, (req:Request, res:Response) => {
   res.status(200).json(res.locals.data)
 })
 export default router
