6c439208d7e4b284d016c3eef55003fb50ce1311
Wvaviator <wvaviator@gmail.com>
2023-01-12 20:31:25 -0600

    remove unusused schema code

--- src/models/trip.ts
+++ src/models/trip.ts
@@ -16,31 +16,25 @@ export interface ITrip {
   endDate: Date;
 }
 
-export const tripSchema = new mongoose.Schema<ITrip>(
-  {
+export const tripSchema = new mongoose.Schema<ITrip>({
+  name: { type: String, required: true },
+  destination: {
     name: { type: String, required: true },
-    destination: {
-      name: { type: String, required: true },
-      location: {
-        lat: { type: Number, required: true },
-        lng: { type: Number, required: true },
-      },
-      place_id: { type: String, required: true },
+    location: {
+      lat: { type: Number, required: true },
+      lng: { type: Number, required: true },
     },
-    packingList: [
-      {
-        name: { type: String, required: true },
-        checked: { type: Boolean, required: true },
-      },
-    ],
-    startDate: { type: Date, required: true },
-    endDate: { type: Date, required: true },
+    place_id: { type: String, required: true },
   },
-  {
-    toObject: { virtuals: true },
-    toJSON: { virtuals: true },
-  }
-);
+  packingList: [
+    {
+      name: { type: String, required: true },
+      checked: { type: Boolean, required: true },
+    },
+  ],
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+});
 
 const Trip =
   (mongoose.models.Trip as mongoose.Model<ITrip>) ||
