8abcd7b0df2f29331519f51a188a3a77699ccbf0
Wvaviator <wvaviator@gmail.com>
2023-01-14 11:09:40 -0600

    fix type declarations

--- src/Globals.d.ts
+++ src/Globals.d.ts
@@ -1,11 +1,4 @@
 declare module '*.css' {
   const content: any;
-  export = content;
-}
-
-import 'express-session';
-declare module 'express-session' {
-  export interface SessionData {
-    userId: string;
-  }
+  export default content;
 }
