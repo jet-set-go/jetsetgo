782793e8d9ad6d4e5705210482093bf4e0dd7459
Wvaviator <wvaviator@gmail.com>
2023-01-14 11:58:45 -0600

    update sort items by checked on load

--- client/components/packinglist/PackingList.tsx
+++ client/components/packinglist/PackingList.tsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { deleteItem } from "./api/deleteItem";
-import { getItems } from "./api/getItems";
-import { createItem } from "./api/createItem";
-import { checkItem } from "./api/checkItem";
-import { TItem } from "./api/getItems";
-import styles from "./PackingList.module.css";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Checkbox from "@mui/material/Checkbox";
-import DeleteIcon from "@mui/icons-material/Delete";
-import LuggageOutlinedIcon from "@mui/icons-material/LuggageOutlined";
+import React, { useState, useEffect } from 'react';
+import { deleteItem } from './api/deleteItem';
+import { getItems } from './api/getItems';
+import { createItem } from './api/createItem';
+import { checkItem } from './api/checkItem';
+import { TItem } from './api/getItems';
+import styles from './PackingList.module.css';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import DeleteIcon from '@mui/icons-material/Delete';
+import LuggageOutlinedIcon from '@mui/icons-material/LuggageOutlined';
 import {
   Card,
   IconButton,
@@ -20,32 +20,32 @@ import {
   TextField,
   CardContent,
   CardMedia,
-} from "@mui/material";
+} from '@mui/material';
 // import Input from "@mui/joy/Input";
-import LuggageIcon from "@mui/icons-material/Luggage";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import { ITrip } from "../../../src/models/trip";
-import { ObjectId } from "mongoose";
-import { orange, amber, cyan, lightBlue } from "@mui/material/colors";
+import LuggageIcon from '@mui/icons-material/Luggage';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { ITrip } from '../../../src/models/trip';
+import { ObjectId } from 'mongoose';
+import { orange, amber, cyan, lightBlue } from '@mui/material/colors';
 
 interface PackingListProps {
   trip: ITrip & { _id: ObjectId };
 }
 
 //oranges
-const primary = orange["A400"];
-const accent = orange["A100"];
+const primary = orange['A400'];
+const accent = orange['A100'];
 const accent2 = amber[500];
 //blues
-const primaryBlue = cyan["A400"];
-const accentBlue = cyan["A100"];
+const primaryBlue = cyan['A400'];
+const accentBlue = cyan['A100'];
 const accent2Blue = cyan[500];
-const accent3Blue = cyan["A900"];
+const accent3Blue = cyan['A900'];
 const PackingList: React.FC<PackingListProps> = ({ trip }) => {
   //set input state
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   //set list state
   const [items, setItems] = useState<TItem[]>(trip.packingList);
 
@@ -63,7 +63,7 @@ const PackingList: React.FC<PackingListProps> = ({ trip }) => {
     setItems(packing);
     //clear out input when done
 
-    setInput("");
+    setInput('');
   }
 
   //for todo list...trying to get it to save to DB
@@ -72,7 +72,7 @@ const PackingList: React.FC<PackingListProps> = ({ trip }) => {
     let checked = newItems.filter((item) => item.checked === true);
     let notchecked = newItems.filter((item) => item.checked !== true);
     let packing2 = notchecked.concat(checked);
-    console.log("pack2", packing2);
+    console.log('pack2', packing2);
     setItems(packing2);
   }
   //handleDelete gets the argument of the item._id
@@ -82,7 +82,7 @@ const PackingList: React.FC<PackingListProps> = ({ trip }) => {
       itemId.toString()
     );
     let rest = remainingItems;
-    console.log("delete", rest);
+    console.log('delete', rest);
     let checked = remainingItems.filter((item) => item.checked === true);
     let notchecked = remainingItems.filter((item) => item.checked !== true);
     let updatedList = notchecked.concat(checked);
@@ -93,7 +93,8 @@ const PackingList: React.FC<PackingListProps> = ({ trip }) => {
   //will grab the items associated with the trip and update everytime it changes
   useEffect(() => {
     async function fetchItems() {
-      const newItems = await getItems(trip._id.toString());
+      const newItems = await getItems(trip.id);
+      newItems.sort((a, b) => (a.checked ? 1 : -1));
       setItems(newItems);
     }
     fetchItems();
@@ -101,32 +102,32 @@ const PackingList: React.FC<PackingListProps> = ({ trip }) => {
 
   //form is for the bottom of the packing list to add another item
   return (
-    <Card sx={{ width: "64%", maxWidth: "100%", minWidth: "250px" }}>
+    <Card sx={{ width: '64%', maxWidth: '100%', minWidth: '250px' }}>
       <CardMedia
         sx={{ height: 70 }}
-        image='https://cdn.packhacker.com/2022/06/d78f3bdf-vpl-flatlay.jpg?auto=compress&auto=format&w=1050&h=700&fit=crop'
+        image="https://cdn.packhacker.com/2022/06/d78f3bdf-vpl-flatlay.jpg?auto=compress&auto=format&w=1050&h=700&fit=crop"
         //image='https://media.istockphoto.com/id/1417417189/vector/vector-set-with-suitcases-bags-and-backpacks-for-travel-different-types-of-hand-luggage.jpg?s=612x612&w=0&k=20&c=D5PAQYQsg18tus_uczB6kmZfNypP_P5OJNi-jalSB3Y='
       ></CardMedia>
       <Grid item xs={3} md={4}>
-        <Typography sx={{ mt: 1, mb: 1 }} variant='subtitle1'>
+        <Typography sx={{ mt: 1, mb: 1 }} variant="subtitle1">
           Packing List:
         </Typography>
 
         <List
           sx={{
-            width: "25%",
-            maxWidth: "100%",
-            height: "40%",
-            position: "fixed",
-            scrollBehavior: "inherit",
-            maxHeight: "100%",
-            overflow: "auto",
-            minWidth: "250px",
+            width: '25%',
+            maxWidth: '100%',
+            height: '40%',
+            position: 'fixed',
+            scrollBehavior: 'inherit',
+            maxHeight: '100%',
+            overflow: 'auto',
+            minWidth: '250px',
           }}
         >
           <CardContent
             sx={{
-              width: "100%",
+              width: '100%',
               bgcolor: primary,
               borderRadius: 1,
             }}
@@ -138,11 +139,11 @@ const PackingList: React.FC<PackingListProps> = ({ trip }) => {
                   key={index}
                   secondaryAction={
                     <IconButton
-                      aria-label='delete'
-                      size='small'
+                      aria-label="delete"
+                      size="small"
                       onClick={() => handleDelete(item._id)}
                     >
-                      <DeleteIcon fontSize='small' />
+                      <DeleteIcon fontSize="small" />
                     </IconButton>
                   }
                   disablePadding
@@ -154,17 +155,17 @@ const PackingList: React.FC<PackingListProps> = ({ trip }) => {
                   >
                     <ListItemIcon>
                       <Checkbox
-                        edge='start'
+                        edge="start"
                         tabIndex={-1}
                         disableRipple
                         icon={<LuggageOutlinedIcon />}
                         checkedIcon={<LuggageIcon />}
-                        inputProps={{ "aria-labelledby": labelId }}
+                        inputProps={{ 'aria-labelledby': labelId }}
                         checked={item.checked}
                         onClick={() => handleCheckItem(item._id)}
                         sx={{
                           color: accent[Symbol],
-                          "&.Mui-checked": {
+                          '&.Mui-checked': {
                             color: accent,
                           },
                         }}
@@ -179,20 +180,20 @@ const PackingList: React.FC<PackingListProps> = ({ trip }) => {
             <div className={styles.packingInput}>
               {<LuggageIcon />}
               <Input
-                placeholder='Lets pack…'
+                placeholder="Lets pack…"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
               />
               <Button
-                value='soft'
-                size='small'
+                value="soft"
+                size="small"
                 className={styles.addButton}
                 onClick={handleCreateItem}
                 sx={{
                   backgroundColor: accent2,
-                  "&:hover": { backgroundColor: accent },
+                  '&:hover': { backgroundColor: accent },
                 }}
-                variant='contained'
+                variant="contained"
               >
                 Add Item
               </Button>
