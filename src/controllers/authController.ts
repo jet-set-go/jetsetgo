f43e2711fb8c0111321b0f06cc882e9c174c8ab4
Wvaviator <wvaviator@gmail.com>
2023-01-14 11:18:45 -0600

    add documentation to auth backend

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,6 +3,13 @@ import { NextFunction, Request, Response } from 'express';
 import User, { IUser } from '../models/userModel';
 import bcrypt from 'bcrypt';
 
+/**
+ * This middleware function will log a user in with the provided email and password. It will then set the user's id on the session and set the user on res.locals.user. It will then call next() to allow the request to continue. If the email or password are not provided, this function will throw an error. If the email is not found in the database, or if the password does not match the password in the database, this function will throw an error.
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
 export const loginWithEmailAndPw = async (
   req: Request,
   res: Response,
@@ -33,6 +40,13 @@ export const loginWithEmailAndPw = async (
   }
 };
 
+/**
+ * This middleware function will register a new user with the provided email and password. It will hash the password before saving it to the database. It will then set the user's id on the session and set the user on res.locals.user. It will then call next() to allow the request to continue.
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
 export const registerWithEmailAndPw = async (
   req: Request,
   res: Response,
@@ -44,7 +58,9 @@ export const registerWithEmailAndPw = async (
       throw new Error('Email and password are required');
     }
 
-    // TODO: Validate email and password
+    // TODO: Validate email is in correct format and password is long enough with required characters
+
+    // TODO: Check if email is already in use
 
     const hashedPassword = await bcrypt.hash(password, 10); //Abc123 -> 83hrg9824hg2h34f89132h4f9h
 
@@ -63,6 +79,13 @@ export const registerWithEmailAndPw = async (
   }
 };
 
+/**
+ * This middleware function closes the gap between OAuth and traditional login. If the user is not using OAuth, the the req.user will be undefined. The req.user will then be populated with the user's information by using the userID property on req.session. If there is no req.session.userID or there is not matching user in the database, this function will not error. It will simply call next() and allow the request to continue. Use this middleware with each request before using any other middleware that accesses user data on the req.user property.
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
 export const getUser = async (
   req: Request,
   res: Response,
@@ -84,6 +107,13 @@ export const getUser = async (
   }
 };
 
+/**
+ * This middleware function will redirect the user to the login page if they are not authenticated. If they are authenticated, it will call next() and allow the request to continue. Note that if the route using this middleware is called by an AJAX request, the user will not be redirected. Instead, the request will fail with a 401 status code.
+ * @param req
+ * @param res
+ * @param next
+ * @returns
+ */
 export const authenticateUser = async (
   req: Request,
   res: Response,
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,23 +1,15 @@
-import express, { NextFunction, Request, Response, Router } from 'express';
+import { Request, Response, Router } from 'express';
 import passport from 'passport';
 import {
   authenticateUser,
   loginWithEmailAndPw,
   registerWithEmailAndPw,
-  // oauthCreateUserOrUpdateSessionIfExists,
 } from '../controllers/authController';
 
 const router = Router();
 
-// interface User extends Express.User {
-//   displayName: string;
-// }
+// Google Auth Routes
 
-//session middleware
-// function isLoggedIn(req: Request, res: Response, next: NextFunction) {
-//   req.user ? next() : res.sendStatus(401);
-// }
-//Login path
 router.get(
   '/google',
   passport.authenticate('google', { scope: ['email', 'profile'] })
@@ -33,27 +25,12 @@ router.get(
   }
 );
 
-// router.get(
-//   '/protected',
-//   isLoggedIn,
-//   oauthCreateUserOrUpdateSessionIfExists,
-//   (req: Request, res: Response) => {
-//     res.send(`Hello, you - ${res.locals.username} - are logged in!`);
-//   }
-// );
-
 router.get('/google/failure', (req: Request, res: Response) => {
   res.send('Failure');
 });
 
-router.get('/signout', (req: Request, res: Response) => {
-  req.logout((err: any) => {
-    return res.status(500).send(err);
-  });
-  res.status(200).send();
-});
 
-//manual login routes
+// Manual Auth Routes
 
 router.post(
   '/register',
@@ -62,14 +39,23 @@ router.post(
   (req: Request, res: Response) => {
     res.status(200).json(res.locals.user);
   }
-);
-
-router.post('/login', loginWithEmailAndPw, (req: Request, res: Response) => {
-  res.status(200).json(res.locals.user);
-});
-
-router.get('/user', authenticateUser, (req: Request, res: Response) => {
-  return res.status(200).json(req.user);
-});
-
+  );
+  
+  router.post('/login', loginWithEmailAndPw, (req: Request, res: Response) => {
+    res.status(200).json(res.locals.user);
+  });
+  
+  // Other Routes
+  
+  router.get('/user', authenticateUser, (req: Request, res: Response) => {
+    return res.status(200).json(req.user);
+  });
+  
+  router.get('/signout', (req: Request, res: Response) => {
+    req.logout((err: any) => {
+      return res.status(500).send(err);
+    });
+    res.status(200).send();
+  });
+  
 export default router;
